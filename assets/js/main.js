d2ab34fd70873d9ba3147e55c776d22b5cd563ae
Jacinthe B <56971054+jabeaudry@users.noreply.github.com>
2021-07-19 02:05:57 -0400

    Navbar fixed
    Navbar now closes
    Additional features such as the final section, no more colourful bars, new scrollbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,21 +9,6 @@ $(document).ready(function() {
 //
 //
 
-//rectangles
-var redBar = document.getElementById("r1");
-var blueBar = document.getElementById("b1");
-var yellowBar = document.getElementById("y1");
-var whiteBarOne = document.getElementById("w1");
-var whiteBarTwo = document.getElementById("w2");
-var whiteBarThree = document.getElementById("w3");
-
-var redBar2 = document.getElementById("r1-s2");
-var blueBar2 = document.getElementById("b1-s2");
-var yellowBar2 = document.getElementById("y1-s2");
-var whiteBarOne2 = document.getElementById("w1-s2");
-var whiteBarTwo2 = document.getElementById("w2-s2");
-var whiteBarThree2 = document.getElementById("w3-s2");
-
 
 //nav
 var menuIcon = document.getElementsByClassName("icon--menu");
@@ -31,14 +16,12 @@ var menuIcon = document.getElementsByClassName("icon--menu");
 //colours
 var black= "#06080f";
 var white= "#f5f5f5";
-var red= "#cc2936";
-var yellow= "#ffb800";
-var blue= "#2364aa";
 
 //picture
 var picBackground = document.getElementsByClassName("picture-background");
 var myPic = document.getElementsByClassName("my-picture");
 var myShortDescription = document.getElementsByClassName("s1-intro");
+var myShortRightDescription = document.getElementsByClassName("s1-intro-right");
 var myShortSubDescription = document.getElementsByClassName("s1-sub-intro");
 var picDescription = document.getElementsByClassName("picture-txt");
 var intro2 = document.getElementsByClassName("intro-2");
@@ -50,6 +33,8 @@ var section3 = document.getElementById("section3");
 var section4 = document.getElementById("section4");
 var section5 = document.getElementById("section5");
 
+//section 2 component
+var s2p2 = document.getElementsByClassName("s2-p2");
 
 
 //
@@ -59,12 +44,16 @@ var section5 = document.getElementById("section5");
 //
 
 
- AOS.init();  //js scroll animation 
+ AOS.init();  //js scroll animation library
 
 
-//fit text
-textFit(document.getElementsByClassName("s1-intro"));
-textFit(document.getElementsByClassName("s1-sub-intro"));
+//closes the navbar when you click on a link
+$('.nav-link').on('click', function(){
+ var check = $(".main-navigation-toggle");
+  check.prop("checked", !check.prop("checked"));
+}); 
+
+
 
 
 //animate()
@@ -89,19 +78,13 @@ if(selection == "section1"){
 
       //animation of rectangles
       
-      s1_t6.fromTo(whiteBarThree, 1, {bottom: "-50vw", opacity: "0", top: "",backgroundColor: white},{bottom: "0", opacity: "1",top: "",backgroundColor: white});
-      s1_t5.fromTo(whiteBarTwo, 0.8, {bottom: "-50vw", opacity: "0", top: "",backgroundColor: white},{bottom: "0", opacity: "1",top: "",backgroundColor: white});
-      s1_t4.fromTo(whiteBarOne, 0.7, {bottom: "-50vw", opacity: "0", top: "",backgroundColor: white},{bottom: "0", opacity: "1",top: "",backgroundColor: white})
-      .fromTo(picDescription, 1, {opacity: "0"}, {opacity: "1"});
-      s1_t3.fromTo(redBar, 0.6, {bottom: "-50vw", opacity: "0", top: ""},{bottom: "0", opacity: "1",top: ""});
-      s1_t2.fromTo(yellowBar, 0.5, {bottom: "-50vw", opacity: "0", top: ""},{bottom: "0", opacity: "1",top: ""})
-      .fromTo(myShortDescription, 1.0, {opacity: "0"}, {opacity: "1"})
-      s1_t2.fromTo(myShortSubDescription, 0.6, {opacity: "0"}, {opacity: "1"});
-      s1_t1.fromTo(blueBar, 0.5, {bottom: "-50vw", opacity: "0", top: ""},{bottom: "0", opacity: "1",top: ""})
-      s1_t7.fromTo(picBackground, 0.4,{width: "3vw", top: "100vh"},{width: "4vw", top: "50vh", ease: Power2.EaseInOut})
-      .fromTo(picBackground,0.4, {width: "3vw"}, {width: "21vw", ease: Power2.EaseInOut})
+      
+      
+     
+      s1_t7.fromTo(picBackground, 0.4,{width: "0vw", top: "100vh", opacity: "0"},{opacity: "0.2", width: "1vw", top: "50vh", ease: Power2.EaseInOut})
+      .fromTo(picBackground,0.4, {opacity: "0.2", width: "1vw"}, {opacity: "1", width: "21vw", ease: Power2.EaseInOut})
       .fromTo(myPic, 0.81, {opacity: "0"}, {opacity: "1"});
-      s1_t20.fromTo(menuIcon, 0.5, {stroke: black}, {stroke: white});
+      s1_t20.fromTo(menuIcon, 0.5, {stroke: white}, {stroke: black});
       console.log("1");
   
     
@@ -120,15 +103,11 @@ if(selection == "section2"){
       s1_t14 = new TimelineMax;
       s2_t20 = new TimelineMax;
 
-	    s1_t13.fromTo(whiteBarThree2, 1, {opacity: "1", top: "0"},{top: "0vh", opacity: "1", backgroundColor: black});
-      s1_t12.fromTo(whiteBarTwo2, 0.8, {opacity: "1", top: "0"},{top: "0vh", opacity: "1", backgroundColor: black});
-      s1_t11.fromTo(whiteBarOne2, 0.7, {opacity: "1", top: "0"},{top: "0vh", opacity: "1", backgroundColor: black});
-      s1_t10.fromTo(redBar2, 0.6, {opacity: "1", top: "-30vh"},{top: "0vh", opacity: "1"});
-      s1_t9.fromTo(yellowBar2, 0.5, {opacity: "1", top: "-30vh"},{top: "0vh", opacity: "1"});
-      s1_t8.fromTo(blueBar2, 0.5, {opacity: "1",top: "-30vh",},{top: "0vh", opacity: "1.0"});
-      s1_t14.fromTo(intro2, 1.2, {opacity: "0", top: "1vh"},{opacity: "1", top: "14vh"} );
+    s1_t14.fromTo(intro2, 1.2, {opacity: "0", top: "1vh"},{opacity: "1", top: "14vh"} );
+    s1_t8.fromTo(s2p2, 1.2, {opacity: "0"}, {opacity:"1"});
      console.log("2");
-     s1_t20.fromTo(menuIcon, 0.5, {stroke: white}, {stroke: black});
+     s2_t20.fromTo(menuIcon, 0.5, {stroke: black}, {stroke: white})
+     .fromTo(picBackground, 0.3, {opacity: "1"}, {opacity: "0"});
    }
    
 }
