3f0846e702bcf53a576ffe12b5835689709e0afb
Jacinthe B <56971054+jabeaudry@users.noreply.github.com>
2021-07-20 01:14:18 -0400

    education and experience
    section 2 added
    includes education and experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,14 +17,6 @@ var menuIcon = document.getElementsByClassName("icon--menu");
 var black= "#06080f";
 var white= "#f5f5f5";
 
-//picture
-var picBackground = document.getElementsByClassName("picture-background");
-var myPic = document.getElementsByClassName("my-picture");
-var myShortDescription = document.getElementsByClassName("s1-intro");
-var myShortRightDescription = document.getElementsByClassName("s1-intro-right");
-var myShortSubDescription = document.getElementsByClassName("s1-sub-intro");
-var picDescription = document.getElementsByClassName("picture-txt");
-var intro2 = document.getElementsByClassName("intro-2");
 
 //sections 
 var section1 = document.getElementById("section1");
@@ -33,9 +25,20 @@ var section3 = document.getElementById("section3");
 var section4 = document.getElementById("section4");
 var section5 = document.getElementById("section5");
 
-//section 2 component
-var s2p2 = document.getElementsByClassName("s2-p2");
 
+//section 1 components
+var picBackground = document.getElementsByClassName("picture-background");
+var myPic = document.getElementsByClassName("my-picture");
+var myShortDescription = document.getElementsByClassName("s1-intro");
+var myShortRightDescription = document.getElementsByClassName("s1-intro-right");
+var myShortSubDescription = document.getElementsByClassName("s1-sub-intro");
+var picDescription = document.getElementsByClassName("picture-txt");
+
+
+//section 2 components
+var s2p2 = document.getElementsByClassName("s2-p2");
+var intro2 = document.getElementsByClassName("intro-2");
+var intro2pt2 = document.getElementsByClassName("s2-intro-right");
 
 //
 //
@@ -103,7 +106,8 @@ if(selection == "section2"){
       s1_t14 = new TimelineMax;
       s2_t20 = new TimelineMax;
 
-    s1_t14.fromTo(intro2, 1.2, {opacity: "0", top: "1vh"},{opacity: "1", top: "14vh"} );
+    s1_t14.fromTo(intro2, 1.2, {opacity: "0", top: "2vh"},{opacity: "1", top: "37vh"} );
+    s1_t13.fromTo(intro2pt2, 1.2, {opacity: "0", top: "15vh"},{opacity: "1", top: "8vh"} );
     s1_t8.fromTo(s2p2, 1.2, {opacity: "0"}, {opacity:"1"});
      console.log("2");
      s2_t20.fromTo(menuIcon, 0.5, {stroke: black}, {stroke: white})
